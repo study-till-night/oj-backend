7ec7911186efc9dd3d0120ba31c5bb113ae4a2f7
study-till-night <zdscg@sina.com>
2024-04-11 14:25:46 +0800

    配置多环境 编写Dockerfile

--- src/main/java/com/shuking/ojbackend/judge/codesandbox/impl/RemoteSandBoxImpl.java
+++ src/main/java/com/shuking/ojbackend/judge/codesandbox/impl/RemoteSandBoxImpl.java
@@ -1,6 +1,5 @@
 package com.shuking.ojbackend.judge.codesandbox.impl;
 
-import cn.hutool.crypto.digest.MD5;
 import cn.hutool.http.HttpUtil;
 import cn.hutool.json.JSONUtil;
 import com.shuking.ojbackend.common.ErrorCode;
@@ -8,18 +7,23 @@ import com.shuking.ojbackend.exception.ThrowUtils;
 import com.shuking.ojbackend.judge.codesandbox.CodeSandBox;
 import com.shuking.ojbackend.judge.codesandbox.model.ExecuteCodeRequest;
 import com.shuking.ojbackend.judge.codesandbox.model.ExecuteCodeResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("remoteSandBox")
+@Slf4j
 public class RemoteSandBoxImpl implements CodeSandBox {
 
-    private final String POST_URL = "http://localhost:8081/sandbox/execute";
 
     //  鉴权请求头名称
     private static final String AUTH_HEADER = "sandbox-header";
     //  密钥字符串
     private static final String AUTH_KEY = "shu-king";
+
+    @Value("${codesandbox.url}")
+    private String POST_URL;
 
     /**
      * @param request 判题服务传输的代码对象
@@ -32,10 +36,11 @@ public class RemoteSandBoxImpl implements CodeSandBox {
 
         // 调用远程沙箱的post请求
         String response = HttpUtil.createPost(POST_URL)
-                .header(AUTH_HEADER,AUTH_KEY).body(JSONUtil.toJsonStr(request)).execute().body();
+                .header(AUTH_HEADER, AUTH_KEY).body(JSONUtil.toJsonStr(request)).execute().body();
 
+        log.info("response:  " + response);
         ThrowUtils.throwIf(StringUtils.isBlank(response), ErrorCode.SYSTEM_ERROR, "代码沙箱服务调用失败");
-        ThrowUtils.throwIf(StringUtils.contains(response,"status: 401"),ErrorCode.NO_AUTH_ERROR,"无权访问代码沙箱");
+        ThrowUtils.throwIf(StringUtils.contains(response, "status: 401"), ErrorCode.NO_AUTH_ERROR, "无权访问代码沙箱");
 
         return JSONUtil.toBean(response, ExecuteCodeResponse.class);
     }
