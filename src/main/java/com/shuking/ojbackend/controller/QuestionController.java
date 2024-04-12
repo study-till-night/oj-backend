855e5ad2ed9ddb08af4ee11760fb74c854ab4aac
study-till-night <zdscg@sina.com>
2024-04-12 09:53:57 +0800

    添加管理员权限

--- src/main/java/com/shuking/ojbackend/controller/QuestionController.java
+++ src/main/java/com/shuking/ojbackend/controller/QuestionController.java
@@ -1,14 +1,15 @@
 package com.shuking.ojbackend.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.google.gson.Gson;
+import com.shuking.ojbackend.annotation.AuthCheck;
 import com.shuking.ojbackend.common.BaseResponse;
 import com.shuking.ojbackend.common.ErrorCode;
 import com.shuking.ojbackend.common.ResultUtils;
+import com.shuking.ojbackend.constant.UserConstant;
 import com.shuking.ojbackend.exception.BusinessException;
 import com.shuking.ojbackend.exception.ThrowUtils;
-import com.shuking.ojbackend.model.dto.question.QuestionAddRequest;
-import com.shuking.ojbackend.model.dto.question.QuestionEditRequest;
-import com.shuking.ojbackend.model.dto.question.QuestionQueryRequest;
+import com.shuking.ojbackend.model.dto.question.*;
 import com.shuking.ojbackend.model.dto.questionsubmit.QuestionSubmitAddRequest;
 import com.shuking.ojbackend.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.shuking.ojbackend.model.entity.Question;
@@ -22,7 +23,10 @@ import com.shuking.ojbackend.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/question")
@@ -37,6 +41,7 @@ public class QuestionController {
     @Resource
     private UserService userService;
 
+    private final static Gson GSON = new Gson();
     // region 增删改查
 
     /**
@@ -59,6 +64,29 @@ public class QuestionController {
 
     /**
      * 根据 id 获取
+     *
+     * @param id    题目id
+     * @return  完整问题对象
+     */
+    @GetMapping("/get")
+    public BaseResponse<Question> getQuestionById(long id, HttpServletRequest request) {
+        if (id <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Question question = questionService.getById(id);
+        if (question == null) {
+            throw new BusinessException(ErrorCode.NOT_FOUND_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        // 不是本人或管理员，不能直接获取所有信息
+        if (!question.getUserId().equals(loginUser.getId()) && !userService.isAdmin(loginUser)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        return ResultUtils.success(question);
+    }
+
+    /**
+     * 根据 id 获取 VO
      *
      * @param id 问题id
      * @return 脱敏后问题对象
@@ -93,6 +121,24 @@ public class QuestionController {
         Page<Question> questionPage = questionService.page(new Page<>(current, size),
                 questionService.getQueryWrapper(questionQueryRequest));
         return ResultUtils.success(questionService.getQuestionVOPage(questionPage, request));
+    }
+
+    /**
+     * 分页获取题目列表（仅管理员）
+     *
+     * @param questionQueryRequest  dto
+     * @param request   http
+     * @return  分页列表
+     */
+    @PostMapping("/list/page")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Page<Question>> listQuestionByPage(@RequestBody QuestionQueryRequest questionQueryRequest,
+                                                           HttpServletRequest request) {
+        long current = questionQueryRequest.getCurrent();
+        long size = questionQueryRequest.getPageSize();
+        Page<Question> questionPage = questionService.page(new Page<>(current, size),
+                questionService.getQueryWrapper(questionQueryRequest));
+        return ResultUtils.success(questionPage);
     }
 
     /**
@@ -134,6 +180,68 @@ public class QuestionController {
         boolean result = questionService.updateQuestion(questionEditRequest, request);
         return ResultUtils.success(result);
     }
+
+    /**
+     * 删除
+     *
+     * @param deleteRequest dto
+     * @param request   http
+     * @return  成功？
+     */
+    @PostMapping("/delete")
+    public BaseResponse<Boolean> deleteQuestion(@RequestBody DeleteRequest deleteRequest, HttpServletRequest request) {
+        if (deleteRequest == null || deleteRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User user = userService.getLoginUser(request);
+        long id = deleteRequest.getId();
+        // 判断是否存在
+        Question oldQuestion = questionService.getById(id);
+        ThrowUtils.throwIf(oldQuestion == null, ErrorCode.NOT_FOUND_ERROR);
+        // 仅本人或管理员可删除
+        if (!oldQuestion.getUserId().equals(user.getId()) && !userService.isAdmin(request)) {
+            throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
+        }
+        boolean b = questionService.removeById(id);
+        return ResultUtils.success(b);
+    }
+
+    /**
+     * 更新（仅管理员）
+     *
+     * @param questionUpdateRequest
+     * @return
+     */
+    @PostMapping("/update")
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    public BaseResponse<Boolean> updateQuestion(@RequestBody QuestionUpdateRequest questionUpdateRequest) {
+        if (questionUpdateRequest == null || questionUpdateRequest.getId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Question question = new Question();
+        BeanUtils.copyProperties(questionUpdateRequest, question);
+        List<String> tags = questionUpdateRequest.getTags();
+        if (tags != null) {
+            question.setTags(GSON.toJson(tags));
+        }
+        List<JudgeCase> judgeCase = questionUpdateRequest.getJudgeCase();
+        if (judgeCase != null) {
+            question.setJudgeCase(GSON.toJson(judgeCase));
+        }
+        JudgeConfig judgeConfig = questionUpdateRequest.getJudgeConfig();
+        if (judgeConfig != null) {
+            question.setJudgeConfig(GSON.toJson(judgeConfig));
+        }
+        // 参数校验
+        questionService.validQuestion(question, false);
+        long id = questionUpdateRequest.getId();
+        // 判断是否存在
+        Question oldQuestion = questionService.getById(id);
+        ThrowUtils.throwIf(oldQuestion == null, ErrorCode.NOT_FOUND_ERROR);
+        boolean result = questionService.updateById(question);
+        return ResultUtils.success(result);
+    }
+
     // endregion
 
 
